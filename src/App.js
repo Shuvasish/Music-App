3f7e0df4870f8a6826e4d305073c9c4d1c438f45
shuvasish <shuvasishtalukdershuvoalways@gmail.com>
2021-04-04 14:17:45 +0600

    mobile responsive

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function App() {
     audioRef.current.play();
   };
   return (
-    <div className="App">
+    <div className={`App ${libraryStatus ? "library-active" : ""}`}>
       <Nav
         libraryStatus={libraryStatus}
         setLibraryStatus={setLibraryStatus}
